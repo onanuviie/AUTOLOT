8a8962f9f405a8b4b0f7e08cd4ab24f79aca2532
onanuviie <nuviesilvet@gmail.com>
2024-01-22 08:09:28 +0100

    linked the two html pages

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const alterdOne = document.querySelector(".altered-one")
 const alterdTwo = document.querySelector(".altered-two")
 
-
 function generateRandom(num1, num2) {
     let difference = num2-num1;
     let rand = Math.random()
